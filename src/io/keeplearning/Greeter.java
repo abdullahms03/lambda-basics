a4412b89a7329645b80b428ef3503f1f31ba3e31
Abdullah <abdullahms03@outlook.com>
2020-10-16 23:19:42 +1100

    Basic print statement

---
+++ src/io/keeplearning/Greeter.java
@@ -0,0 +1,14 @@
+package io.keeplearning;
+
+public class Greeter {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet();
+    }
+
+    public void greet() {
+        System.out.println("Hello World!");
+    }
+
+}
