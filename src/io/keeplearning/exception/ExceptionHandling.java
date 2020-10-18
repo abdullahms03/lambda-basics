77db138629a97e50a41a2005ab24503569c5b497
Abdullah <abdullahms03@outlook.com>
2020-10-18 23:33:34 +1100

    Exception Handling - Clumsy option

--- src/io/keeplearning/exception/ExceptionHandling.java
+++ src/io/keeplearning/exception/ExceptionHandling.java
@@ -7,17 +7,18 @@ public class ExceptionHandling {
     public static void main(String[] args) {
         int[] numbers = {2, 4, 6, 8};
         int key = 0;
-        process(numbers, key, (v, k) -> System.out.println("Output : " + (v / k)));
+        process(numbers, key, (v, k) -> {
+            try {
+                System.out.println("Output : " + (v / k));
+            } catch (ArithmeticException e) {
+                System.out.println("Arithmetic exception occurred : " + e.getMessage());
+            }
+        });
     }
 
     private static void process(int[] numbers, int key, BiConsumer<Integer, Integer> consumer) {
         for (int val : numbers) {
-            try {
-                consumer.accept(val, key);
-            } catch (ArithmeticException e) {
-                // The behaviour passed can be anything, so handling specific exception here is not a advisable option
-                System.out.println("Arithmetic exception occurred : " + e.getMessage());
-            }
+            consumer.accept(val, key);
         }
     }
 }
